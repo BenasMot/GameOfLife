8c4dd8b83d2424121bb6c41c8a6f66c5c9268898
BenasMot <benasmotiejunas.bm@gmail.com>
2021-12-30 18:58:51 +0200

    basic display and test of features in main

--- main.cpp
+++ main.cpp
@@ -10,19 +10,45 @@ using namespace std;
 
 int main() {
   GameOfLife game;
-  
+
   Grid init;
-  init[make_pair(1,1)] = new Cell(true);
-  init[make_pair(1,2)] = new Cell(true);
-  init[make_pair(1,3)] = new Cell(true);
+  init[pair{1, 1}] = new Cell(true);
+  init[pair{1, 2}] = new Cell(true);
+  init[pair{1, 3}] = new Cell(true);
   game.initialize(init);
 
-  game.addCell(make_pair(2,2));
+  // game.addCell(pair{2, 2});
+  game.update();
+
+  int x_max = 6, y_max = 6;
+  Grid table;
+  for (int i = 0; i < x_max; i++) {
+    for (int j = 0; j < y_max; j++) {
+      table[pair{i, j}] = new Cell(false);
+    }
+  }
 
   Grid a = game.getCells();
-  for (auto cell:a){
-    cout << "[" << cell.first.first << ", " << cell.first.second << "] " << cell.second->getIsAlive() << endl;
+  // for (auto cell : a) {
+  //   cout << "[" << cell.first.first << ", " << cell.first.second << "] " << cell.second->getIsAlive() << " " << cell.second->getNearbyCells() << endl;
+  // }
+  a.merge(table);
+
+  cout << "---------" << endl;
+  for (int i = 0; i < x_max; i++) {
+    for (int j = 0; j < y_max; j++) {
+      cout << a[pair{i, j}]->getIsAlive() << " ";
+    }
+    cout << endl;
   }
+  cout << "---------" << endl;
+  for (int i = 0; i < x_max; i++) {
+    for (int j = 0; j < y_max; j++) {
+      cout << a[pair{i, j}]->getNearbyCells() << " ";
+    }
+    cout << endl;
+  }
+  cout << "---------" << endl;
 
   return 0;
 }
