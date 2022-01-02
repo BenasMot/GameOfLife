b54f4eeb3e7b75871e7a510380349b5d19820c22
BenasMot <benasmotiejunas.bm@gmail.com>
2022-01-02 20:21:19 +0200

    add timeout to timer

--- src/Timer.hpp
+++ src/Timer.hpp
@@ -2,6 +2,7 @@
 #define TIMER_H
 
 #include <chrono>
+#include <thread>
 
 class Timer {
  private:
@@ -12,6 +13,7 @@ class Timer {
   void start() { start_time = std::chrono::high_resolution_clock::now(); }
   void stop() { stop_time = std::chrono::high_resolution_clock::now(); }
   int get_elapsed() { return std::chrono::duration_cast<std::chrono::microseconds>(stop_time - start_time).count(); }
+  void setTimeout(int ms) { std::this_thread::sleep_for(std::chrono::milliseconds(ms)); }
 };
 
 #endif
